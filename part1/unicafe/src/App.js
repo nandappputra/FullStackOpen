503640f48b7cddca9c62f1dd68b1c8174e7be329
nandappputra <60767596+nandappputra@users.noreply.github.com>
2021-09-10 16:08:18 +0700

    Fixed Error on Exercise 1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -24,10 +24,12 @@ const StatisticLine = ({text,value}) =>{
   }
 
   return(
-    <tr>
-      <td>{text}</td>
-      <td>{value}</td>
-    </tr>
+    <>
+      <tr>
+        <td>{text}</td>
+        <td>{value}</td>
+      </tr>
+    </>
   )
 }
 
@@ -41,12 +43,14 @@ const Statistics = ({good,neutral,bad}) =>{
     <div>
       <h1>Statistics</h1>
       <table>
-        <StatisticLine text="good" value={good}/>
-        <StatisticLine text="neutral" value={neutral}/>
-        <StatisticLine text="bad" value={bad}/>
-        <StatisticLine text="all" value={good+neutral+bad}/>
-        <StatisticLine text="average" value={(good-bad)/(good+neutral+bad)}/>
-        <StatisticLine text="positive" value={(good)/(good+neutral+bad)*100}/>
+        <tbody>
+          <StatisticLine text="good" value={good}/>
+          <StatisticLine text="neutral" value={neutral}/>
+          <StatisticLine text="bad" value={bad}/>
+          <StatisticLine text="all" value={good+neutral+bad}/>
+          <StatisticLine text="average" value={(good-bad)/(good+neutral+bad)}/>
+          <StatisticLine text="positive" value={(good)/(good+neutral+bad)*100}/>
+        </tbody>
       </table>
     </div>
   )
