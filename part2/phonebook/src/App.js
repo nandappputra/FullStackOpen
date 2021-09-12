83086edb19217babdbf755129658f0078cabaa8e
nandappputra <60767596+nandappputra@users.noreply.github.com>
2021-09-12 13:54:40 +0700

    Added Exercise 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,11 +1,16 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number:'08080808080' }
-  ]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456'},
+    { name: 'Ada Lovelace', number: '39-44-5323523'},
+    { name: 'Dan Abramov', number: '12-43-234345'},
+    { name: 'Mary Poppendieck', number: '39-23-6423122'}
+  ])
+
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ searchQuery, setSearchQuery ] = useState('')
 
   const nameHandler = (event) => {
     setNewName(event.target.value)
@@ -13,6 +18,10 @@ const App = () => {
 
   const numberHandler = (event) => {
     setNewNumber(event.target.value)
+  }
+
+  const updateSearch = (event) => {
+    setSearchQuery(event.target.value)
   }
   
   const alreadyExist = (name) => {
@@ -40,13 +49,15 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div> filter shown with <input value={searchQuery} onChange={updateSearch}/></div>
+      <h2>Add a new</h2>
       <form onSubmit={saveContact}>
         <div> name: <input value={newName} onChange={nameHandler}/></div>
         <div> number: <input value={newNumber} onChange={numberHandler}/></div>
         <div><button type="submit">add</button></div>
       </form>
       <h2>Numbers</h2>
-      {persons.map( (person) =>
+      {persons.filter( (person)=> person.name.toLowerCase().includes(searchQuery)).map( (person) =>
         <p key={person.name}>{person.name} {person.number}</p>
       )}
     </div>
@@ -54,3 +65,4 @@ const App = () => {
 }
 
 export default App
+
