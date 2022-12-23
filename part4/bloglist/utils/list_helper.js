ddff81536bbc695ca6d43208037e85efcd7945b7
Nanda Pratama Putra <60767596+nandappputra@users.noreply.github.com>
2022-12-23 17:23:58 +0700

    Add exercise 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  const likeSum = (sum, blog) => {
+    return sum + blog.likes;
+  };
+
+  return blogs.reduce(likeSum, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
