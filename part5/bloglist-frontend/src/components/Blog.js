587788de7e9fb519c731c5d6dfe473394ca0013a
Nanda Pratama Putra <60767596+nandappputra@users.noreply.github.com>
2022-12-30 16:49:21 +0700

    Add exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,44 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    backgroundColor: "lightgrey",
+    borderRadius: "0.5em",
+    padding: "0.5em",
+    margin: "1em",
+  };
+  const [detailVisible, setDetailVisibility] = useState(false);
+  const [buttonText, setButtonText] = useState("view");
+
+  const toggleDetail = () => {
+    if (detailVisible) {
+      setButtonText("view");
+    } else {
+      setButtonText("hide");
+    }
+    setDetailVisibility(!detailVisible);
+  };
+
+  const blogDetail = () => {
+    return (
+      <div>
+        {blog.url} <br />
+        {blog.likes} <button>like</button>
+        <br />
+        {blog.name}
+      </div>
+    );
+  };
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button type="button" onClick={toggleDetail}>
+        {buttonText}
+      </button>
+      {detailVisible && blogDetail()}
+    </div>
+  );
+};
+
+export default Blog;
