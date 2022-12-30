bf2cee4b1a223ba1e07510f5adf3c38bbb098fd5
Nanda Pratama Putra <60767596+nandappputra@users.noreply.github.com>
2022-12-30 22:00:48 +0700

    Add exercise 5.14

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Blog from "./Blog";
 
 describe("Blog", () => {
@@ -24,5 +25,31 @@ describe("Blog", () => {
     expect(element.textContent).toContain("me");
     expect(element.textContent).not.toContain("30");
     expect(element.textContent).not.toContain("hehehe.com");
+  });
+
+  test("should render likes and url after the view button is clicked", async () => {
+    const blog = {
+      title: "testing",
+      author: "me",
+      likes: 30,
+      url: "hehehe.com",
+    };
+
+    const mockRemoveBlogFromList = jest.fn();
+
+    const { container } = render(
+      <Blog blog={blog} removeBlogFromList={mockRemoveBlogFromList} />
+    );
+
+    const user = userEvent.setup();
+    const button = screen.getByText("view");
+    await user.click(button);
+
+    const element = container.querySelector(".blog");
+
+    expect(element.textContent).toContain("testing");
+    expect(element.textContent).toContain("me");
+    expect(element.textContent).toContain("30");
+    expect(element.textContent).toContain("hehehe.com");
   });
 });
