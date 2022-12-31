67a1e38e2afd24499610c648fdb96829c09f1bff
Nanda Pratama Putra <60767596+nandappputra@users.noreply.github.com>
2022-12-31 14:38:54 +0700

    Add exercise 5.17

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -21,6 +21,12 @@ mongoose.connect(mongoUrl).then(() => {
 app.use(cors());
 app.use(express.json());
 app.use(middleware.tokenExtractor);
+
+if (process.env.NODE_ENV === "test") {
+  const testingRouter = require("./controllers/testing");
+  app.use("/api/testing", testingRouter);
+}
+
 app.use("/api/login", loginRouter);
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
