998437245fc200b099570845625041bf64d3c3c2
Nanda Pratama Putra <60767596+nandappputra@users.noreply.github.com>
2023-01-13 23:34:04 +0700

    Add exercise 6.21

--- part6/redux-anecdotes/src/reducers/notificationReducer.js
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -2,13 +2,17 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const notificationSlice = createSlice({
   name: "notification",
-  initialState: "hello",
+  initialState: {
+    message: "hello",
+    latestTimeout: null,
+  },
   reducers: {
     setNotification(state, action) {
+      clearTimeout(state.latestTimeout);
       return action.payload;
     },
     clearNotification(state, action) {
-      return null;
+      return { ...state, message: null };
     },
   },
 });
@@ -17,8 +21,15 @@ export const { setNotification, clearNotification } = notificationSlice.actions;
 
 export const setNotificationWithTimeout = (message, timeout) => {
   return async (dispatch) => {
-    dispatch(setNotification(message));
-    setTimeout(() => dispatch(clearNotification()), timeout);
+    let latestTimeout = setTimeout(
+      () => dispatch(clearNotification()),
+      timeout
+    );
+    let payload = {
+      message,
+      latestTimeout,
+    };
+    dispatch(setNotification(payload));
   };
 };
 
