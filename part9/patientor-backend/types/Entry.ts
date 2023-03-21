bf9e3a18cd37f0561c133481812553f303b90c6b
Nanda Pratama Putra <60767596+nandappputra@users.noreply.github.com>
2023-03-21 10:49:21 +0700

    Add exercise 9.22

--- part9/patientor-backend/types/Entry.ts
+++ part9/patientor-backend/types/Entry.ts
@@ -1,2 +1,44 @@
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {}
+export type Entry =
+  | OccupationalHealthcareEntry
+  | HospitalEntry
+  | HealthCheckEntry;
+
+export interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+export interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+export interface OccupationalHealthcareEntry {
+  id: string;
+  date: string;
+  type: string;
+  specialist: string;
+  employerName: string;
+  diagnosisCodes?: Array<string>;
+  description: string;
+  sickLeave?: SickLeave;
+}
+
+export interface HospitalEntry {
+  id: string;
+  date: string;
+  type: string;
+  specialist: string;
+  diagnosisCodes: Array<string>;
+  description: string;
+  discharge: Discharge;
+}
+
+export interface HealthCheckEntry {
+  id: string;
+  date: string;
+  specialist: string;
+  type: string;
+  description: string;
+  healthCheckRating: number;
+}
